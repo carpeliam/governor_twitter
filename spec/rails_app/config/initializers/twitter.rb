c2023013063853d6e0d5dc88f6d005adfb82164d
Liam Morley <liam@carpeliam.com>
2011-05-15 04:39:51 -0400

    initializer not really needed

--- spec/rails_app/config/initializers/twitter.rb
+++
@@ -1,6 +0,0 @@
-Twitter.configure do |config|
-  config.consumer_key = 'x2kAIZqhVbB8aYBkbIhLA'
-  config.consumer_secret = 'N3m27LRWN6kglvJ2QIoqS6HKv0y1MBqFNMBUpEl8g'
-  config.oauth_token = '298776110-A4adk6Whxn6ROReYsNyx2Ate948qXS2hQpIGVAkg'
-  config.oauth_token_secret = 'C2eeyC3R28PjNlaXAZXmr9hKC1MvBZy0A3wawO4xI'
-end
